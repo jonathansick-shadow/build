d949780df44eb06f455047e180cc2734bd973859
rplante <rplante@git.lsstcorp.org>
2007-03-06 18:28:16 +0000

    updated for reorganized stack

--- deploy/LSSTinit/loadLSST.csh
+++ deploy/LSSTinit/loadLSST.csh
@@ -8,24 +8,36 @@ if (! $?LSST_HOME) then
     # setenv LSST_HOME $PWD
 endif
 
-if (! $?EUPS_FLAVOR) then
-    # set the EUPS_FLAVOR 
-    # setenv EUPS_FLAVOR Linux
-endif 
-
 if (! $?EUPS_PKGROOT) then
     # set the EUPS_PKGROOT 
     # setenv EUPS_PKGROOT http://lsstdev.ncsa.uiuc.edu/pkgs
 endif 
 
 # Load PACMAN
-source $LSST_HOME/external/pacman/latest/setup.csh
+source $LSST_HOME/pacman/latest/setup.csh
 
 # Load EUPS
-source $LSST_HOME/external/eups/latest/bin/setups.csh
-echo $EUPS_PATH | sed -e 's/:/\n/g' | egrep ^${LSST_HOME}$
+source $LSST_HOME/eups/latest/bin/setups.csh
+
+# make sure LSST_HOME is part of the EUPS path
+set target = `ls -id1 $LSST_HOME | awk '{print $1}'`
+ls -id1 `echo $EUPS_PATH | sed -e 's/:/ /g'` | egrep -q "^${target} "
 if ($status != 0) then
     setenv EUPS_PATH ${LSST_HOME}:$EUPS_PATH
+endif
+
+# make sure LSST_DEVEL is part of the EUPS path
+if ($?LSST_DEVEL) then
+    if (-e "$LSST_DEVEL/ups_db") then
+        set target = `ls -id1 $LSST_DEVEL | awk '{print $1}'`
+        ls -id1 `echo $EUPS_PATH | sed -e 's/:/ /g'` | egrep -q "^${target} "
+        if ($status != 0) then
+            setenv EUPS_PATH "${LSST_DEVEL}:$EUPS_PATH"
+        endif
+    else 
+        echo "LSST_DEVEL=${LSST_DEVEL}: "
+        echo "    Not setup for EUPS (not ups_db directory); ignoring..."
+    endif
 endif
 
 # Setup your default environemnt
--- deploy/LSSTinit/loadLSST.sh
+++ deploy/LSSTinit/loadLSST.sh
@@ -3,31 +3,42 @@
 #  Source this file directly or copy it to your own directory for 
 #  modifications. 
 #
-if [ -z $LSST_HOME ]; then
+if [ -z "$LSST_HOME" ]; then
     # set the LSST_HOME directory
     # export LSST_HOME=$PWD
 fi
 
-if [ -z $EUPS_FLAVOR ]; then
-    # set the EUPS_FLAVOR 
-    # export EUPS_FLAVOR=Linux
-fi 
-
-if [ -z $EUPS_PKGROOT ]; then
+if [ -z "$EUPS_PKGROOT" ]; then
     # set the EUPS_PKGROOT 
-    # export EUPS_PKGROOT=http://lsstdev.ncsa.uiuc.edu/pkgs
+    # export EUPS_PKGROOT=http://dev.lsstcorp.org/pkgs
 fi 
 
 # Load PACMAN
-. $LSST_HOME/external/pacman/latest/setup.sh
+. $LSST_HOME/pacman/latest/setup.sh
 
 # Load EUPS
-. $LSST_HOME/external/eups/latest/bin/setups.sh
-echo $EUPS_PATH | sed -e 's/:/\n/g' | egrep ^${LSST_HOME}$
-if [ $? != 0 ]; then
-    export EUPS_PATH=${LSST_HOME}:$EUPS_PATH
-endif
+. $LSST_HOME/eups/latest/bin/setups.sh
 
+# make sure LSST_HOME is part of the EUPS path
+target=`ls -id1 $LSST_HOME | awk '{print $1}'`
+ls -id1 `echo $EUPS_PATH | sed -e 's/:/ /g'` | egrep -q "^${target} "
+if [ $? != 0 ]; then
+    export EUPS_PATH="${LSST_HOME}:$EUPS_PATH"
+fi
+
+# make sure LSST_DEVEL is part of the EUPS path
+if [ -n "$LSST_DEVEL" ]; then
+    if [ -e "$LSST_DEVEL/ups_db" ]; then
+        target=`ls -id1 $LSST_DEVEL | awk '{print $1}'`
+        ls -id1 `echo $EUPS_PATH | sed -e 's/:/ /g'` | egrep -q "^${target} "
+        if [ $? != 0 ]; then
+            export EUPS_PATH="${LSST_DEVEL}:$EUPS_PATH"
+        fi  
+    else 
+        echo "LSST_DEVEL=${LSST_DEVEL}: "
+        echo "    Not setup for EUPS (no ups_db directory); ignoring..."
+    fi
+fi
 
 # Setup your default environemnt
 # setup LSST 1.0
